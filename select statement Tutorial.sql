ddacf17cb883085aa995001f875a94c23ec73fd2
muhammadkhan2200 <168642564+muhammadkhan2200@users.noreply.github.com>
2024-09-03 15:20:10 -0700

    Add files via upload

---
+++ select statement Tutorial.sql
@@ -0,0 +1,14 @@
+select*
+From parks_and_recreation. employee_demographics;
+
+
+
+
+
+
+select first_name, 
+last_name, 
+birth_date
+age,
+(age + 10) * 10
+From parks_and_recreation. employee_demographics;
